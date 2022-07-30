c980dc1451a4f3e9dd0c176b93cae6c2c5050e93
Fred <guru.three@gmail.com>
2022-07-30 11:56:48 +0100

    pio init added

--- mac.c
+++ mac.c
@@ -1,6 +1,36 @@
+/*
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 guruthree
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "videoinput.pio.h"
+
+// someone doing something similar with the gameboy
+// https://old.reddit.com/r/raspberrypipico/comments/lux5k6/pio_issue_attempting_to_capture_gameboy_screen/
 
 // https://shop.pimoroni.com/products/pimoroni-pico-vga-demo-base?variant=32369520672851
 
@@ -14,18 +44,39 @@
 // scanvideo_minimal and test_pattern seem to be the easiest to interpret resources?
 // https://blog-boochow-com.translate.goog/article/raspberry-pi-pico-vga.html?_x_tr_sl=ja&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=sc
 
+// 60 Hz * 22.25 KHz = 1.335 MHz
+
+#define CLOCK_SPEED 120e6
+#define CLOCK_DIV 90 // at 120 MHz, every 90 samples would be 1.333 MHz
+
 uint64_t a = 0, b = 0;
 
+#define SYNC_PIN 2 // pin 4
+
+
+// this should wait for an h-sync
+// mark data to be sent
+// then disable itself
 void gpio_callback(uint gpio, uint32_t events) {
     a = to_us_since_boot(get_absolute_time());
     printf("on time: %lld\n", a - b);
     b = a;
 }
 
-#define SYNC_PIN 2 // pin 4
-
 int main() {
     stdio_init_all(); // need to get VGA output working so that there's some isolation from the PC?
+
+    // setup the DAC
+    PIO pio = pio0;
+    int pio_sm = 0;
+    int pio_offset;
+
+    // init PIO
+    pio_offset = pio_add_program(pio, &videoinput_program);
+    videoinput_program_init(pio, pio_sm, pio_offset, 0, CLOCK_DIV);
+
+    // setup DMA
+    uint8_t dma_channel;
 
     sleep_ms(5000);
     printf("hello world\n");
