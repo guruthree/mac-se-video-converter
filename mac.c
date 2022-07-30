0fb7e7507d596f78c04398a9b122f73a70cffdb6
Fred <guru.three@gmail.com>
2022-07-30 18:52:19 +0100

    getting a full picture, but tilted

--- mac.c
+++ mac.c
@@ -39,6 +39,7 @@
 
 // http://www.waveguide.se/?article=compact-mac-video-adapter
 // need to read 704(+14+164?) bit between 22.25 kHz pulses?
+// https://tinkerdifferent.com/threads/warpse-25-mhz-68hc000-based-accelerator-for-mac-se.253/page-11#post-10080
 
 // scanvideo or scanvideo_dpi for doing VGA
 // seems to let you write out a line at a time of video?
@@ -49,15 +50,26 @@
 
 // 60 Hz * 22.25 KHz = 1.335 MHz
 
-#define CLOCK_SPEED 120e6
-#define CLOCK_DIV 90 // at 120 MHz, every 90 samples would be 1.333 MHz
+// 1.1 ms vertical sync pulse?
+// 0.045 ms per horizontal line? (22.22 KHz vs 22.25 KHz?)
+
+// (CLOCK/DV)^-1*SAMPLES*1000 = 1/horizontal sync 
+// sample freq = 28,577,777.77?
+// 1286 samples? 
+
+#define CLOCK_SPEED 142e6
+#define CLOCK_DIV 4.968895801 // at 120 MHz, every 90 samples would be 1.333 MHz
 
 #define HSYNC_PIN 20 // pin 26
 #define VSYNC_PIN 19 // pin 25
 #define VIDEO_PIN 18 // pin 24
 
 // buffer needs to be multiple of 4, 88*8 = 704 bits
-#define BUFFER_LEN_32 22
+// 4096 took 4576 us
+
+// about 370 lines?
+
+#define BUFFER_LEN_32 14869
 #define BUFFER_LEN_8 BUFFER_LEN_32*4
 uint8_t buffer[BUFFER_LEN_8];
 bool dataready = false;
@@ -70,17 +82,18 @@ uint8_t dma_channel;
 
 uint64_t a = 0, b = 0;
 void gpio_callback(uint gpio, uint32_t events) {
-    if (gpio == HSYNC_PIN) {
-        gpio_set_irq_enabled(HSYNC_PIN, GPIO_IRQ_EDGE_RISE, false);
+//    if (gpio == VSYNC_PIN) {
+//        gpio_set_irq_enabled(HSYNC_PIN, GPIO_IRQ_EDGE_RISE, false);
+        gpio_set_irq_enabled(VSYNC_PIN, GPIO_IRQ_EDGE_RISE, false);
         gpio_put(PICO_DEFAULT_LED_PIN, true);
         dma_channel_set_write_addr(dma_channel, buffer, true);
         dataready = true;
-    }
-    else if (gpio == VSYNC_PIN) {
+//    }
+//    else if (gpio == VSYNC_PIN) {
         a = to_us_since_boot(get_absolute_time());
-        printf("frame time (us): %lld\n", (a - b));
-        b = a;
-    }
+//        printf("frame time (us): %lld\n", (a - b));
+//        b = a;
+//    }
 }
 
 int main() {
@@ -122,39 +135,59 @@ int main() {
     gpio_init(HSYNC_PIN);
     gpio_set_dir(HSYNC_PIN, GPIO_IN);
     gpio_pull_down(HSYNC_PIN); // mac defaults to pulling up
-    gpio_set_irq_enabled_with_callback(HSYNC_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+//    gpio_set_irq_enabled_with_callback(HSYNC_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
 
     // setup interrupt on vsync
     gpio_init(VSYNC_PIN);
     gpio_set_dir(VSYNC_PIN, GPIO_IN);
     gpio_pull_down(VSYNC_PIN); // mac defaults to pulling up
-    gpio_set_irq_enabled(VSYNC_PIN, GPIO_IRQ_EDGE_RISE, true);
-//    gpio_set_irq_enabled_with_callback(VSYNC_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+//    gpio_set_irq_enabled(VSYNC_PIN, GPIO_IRQ_EDGE_RISE, true);
+    gpio_set_irq_enabled_with_callback(VSYNC_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
 
-    char temp[10];
+//    char temp[10];
 
     while (1) {
+//        sleep_ms(100);
         sleep_us(1);
 //        tight_loop_contents();
         if (dataready) {
             printf("waiting\n");
             dma_channel_wait_for_finish_blocking(dma_channel);
+            printf("capture duration (us): %lld\n", (to_us_since_boot(get_absolute_time()) - a));
 
-
-            for (uint8_t i = 0; i < BUFFER_LEN_8; i++) {
-                memset(temp, 0, 10);
+            uint32_t at = 0;
+            for (uint32_t i = 0; i < BUFFER_LEN_8; i++) {
+//                memset(temp, 0, 10);
                 for (uint8_t j = 0; j < 8; j ++) {
-                    sprintf(&temp[j], "%d", (buffer[i] & (1 << j)) >> j);
+//                    sprintf(&temp[j], "%d", (buffer[i] & (1 << j)) >> j);
+
+                    if (((buffer[i] & (1 << j)) >> j) == 0) {
+                        printf("0");
+                    }
+                    else {
+                        printf("1");
+                    }
+
+                    at++;
+                    if (at >= 1286) {
+                        at = 0;
+                        printf("\n");
+                    }
+
                 }
-                printf(temp);
+//                printf(temp);
+//                at++;
+//                if (at >= 88) {
+//                    at = 0;
+//                    printf("\n");
+//                }
             }
 
             dataready = false;
             printf("sleeping\n");
-            sleep_ms(1000);
+            sleep_ms(5000);
             printf("interrupt enable\n");
-            gpio_set_irq_enabled(HSYNC_PIN, GPIO_IRQ_EDGE_RISE, true);
-//            gpio_set_irq_enabled_with_callback(HSYNC_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+            gpio_set_irq_enabled(VSYNC_PIN, GPIO_IRQ_EDGE_RISE, true);
             gpio_put(PICO_DEFAULT_LED_PIN, false);
         }
 
